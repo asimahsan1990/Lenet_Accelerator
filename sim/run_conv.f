7f0a95fe890d72573aca0e0ac8f12578ac607528
Paul Lai <paul116e@gmail.com>
2017-12-29 22:45:32 +0800

    update run_conv.f

--- sim/run_conv.f
+++ sim/run_conv.f
@@ -3,10 +3,10 @@ test_conv.v
 ../hdl/conv_hdl/conv_top.v
 ../hdl/conv_hdl/fsm.v
 ../hdl/conv_hdl/conv_control.v
-../hdl/conv_hdl/conv_bias_sel.v
-../hdl/conv_hdl/conv_data_reg.v
-../hdl/conv_hdl/conv_multiply_compare.v
-../hdl/conv_hdl/conv_quantize.v
+../hdl/conv_hdl/bias_sel.v
+../hdl/conv_hdl/data_reg.v
+../hdl/conv_hdl/multiply_compare.v
+../hdl/conv_hdl/quantize.v
 
 sram_model/sram_256x32b.v
 sram_model/sram_20000x100b.v
