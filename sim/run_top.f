6095564b1eec6b2c248e7815825aaa832d451423
Jason Lo <tarzen0509@gmail.com>
2018-01-01 01:31:14 +0800

    update run_top.f

--- sim/run_top.f
+++ sim/run_top.f
@@ -1,19 +1,20 @@
 test_top.v
 
-CONV1/conv1_top.v
-CONV1/conv1_controll.v
-CONV1/data_reg.v
-CONV1/quantize.v
-CONV1/multiply_compare.v
-CONV1/bias_sel.v
+../hdl/conv_hdl/conv_top.v
+../hdl/conv_hdl/fsm.v
+../hdl/conv_hdl/conv_control.v
+../hdl/conv_hdl/bias_sel.v
+../hdl/conv_hdl/data_reg.v
+../hdl/conv_hdl/multiply_compare.v
+../hdl/conv_hdl/quantize.v
 
-../hdl/conv2_top.v
-../hdl/conv2_control.v
-../hdl/conv2_bias_sel.v
-../hdl/conv2_data_reg.v
-../hdl/conv2_multiply_compare.v
-../hdl/conv2_quantize.v
+../hdl/fc_hdl/fc_top.v
+../hdl/fc_hdl/fc_controller.v
+../hdl/fc_hdl/data_reg.v
+../hdl/fc_hdl/multiplier_accumulator.v
+../hdl/fc_hdl/quantize.v
 
 sram_model/sram_256x32b.v
 sram_model/sram_20000x100b.v
+sram_model/sram_20250x80b.v
 +access+r 
