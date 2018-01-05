79a609abadef2ea1dcb60708f1d05d5839b7dfeb
Jason Lo <tarzen0509@gmail.com>
2018-01-05 16:01:13 +0800

    added files

--- icc/pre_layout/design_data/CHIP_syn_app.v
+++ icc/pre_layout/design_data/CHIP_syn_app.v
@@ -1,3 +1,4 @@
+/* Author : Yunchen Lo */
 //Wrap the synthesized netlist 
 
 module CHIP ( clk, srstn, conv_start, conv_done, mem_sel, fc1_done, fc2_done,
@@ -194,6 +195,7 @@ module CHIP ( clk, srstn, conv_start, conv_done, mem_sel, fc1_done, fc2_done,
   input [31:0] sram_rdata_a6;
   input [31:0] sram_rdata_a7;
   input [31:0] sram_rdata_a8;
+
   output [9:0] sram_raddr_a0;
   output [9:0] sram_raddr_a1;
   output [9:0] sram_raddr_a2;
@@ -203,6 +205,7 @@ module CHIP ( clk, srstn, conv_start, conv_done, mem_sel, fc1_done, fc2_done,
   output [9:0] sram_raddr_a6;
   output [9:0] sram_raddr_a7;
   output [9:0] sram_raddr_a8;
+
   input [31:0] sram_rdata_a0_1;
   input [31:0] sram_rdata_a1_1;
   input [31:0] sram_rdata_a2_1;
@@ -222,6 +225,7 @@ module CHIP ( clk, srstn, conv_start, conv_done, mem_sel, fc1_done, fc2_done,
   input [31:0] sram_rdata_b6;
   input [31:0] sram_rdata_b7;
   input [31:0] sram_rdata_b8;
+  
   output [9:0] sram_raddr_b0;
   output [9:0] sram_raddr_b1;
   output [9:0] sram_raddr_b2;
@@ -353,11 +357,194 @@ module CHIP ( clk, srstn, conv_start, conv_done, mem_sel, fc1_done, fc2_done,
   input [79:0] fc_sram_rdata_weight;
   output [14:0] fc_sram_raddr_weight;
 
+lenet_2set
+U0 (
+  .clk(clk),
+  .srstn(srstn),
+/* CONTROL SIGNALS */
+  .conv_start(conv_start),
+  .conv_done(conv_done),
+  .mem_sel(mem_sel),
+  .fc1_done(fc1_done),
+  .fc2_done(fc2_done),
+/* SRAM A */
+  .sram_rdata_a0(sram_rdata_a0),
+  .sram_rdata_a1(sram_rdata_a1),
+  .sram_rdata_a2(sram_rdata_a2),
+  .sram_rdata_a3(sram_rdata_a3),
+  .sram_rdata_a4(sram_rdata_a4),
+  .sram_rdata_a5(sram_rdata_a5),
+  .sram_rdata_a6(sram_rdata_a6),
+  .sram_rdata_a7(sram_rdata_a7),
+  .sram_rdata_a8(sram_rdata_a8),
 
+  .sram_rdata_a0_1(sram_rdata_a0_1),
+  .sram_rdata_a1_1(sram_rdata_a1_1),
+  .sram_rdata_a2_1(sram_rdata_a2_1),
+  .sram_rdata_a3_1(sram_rdata_a3_1),
+  .sram_rdata_a4_1(sram_rdata_a4_1),
+  .sram_rdata_a5_1(sram_rdata_a5_1),
+  .sram_rdata_a6_1(sram_rdata_a6_1),
+  .sram_rdata_a7_1(sram_rdata_a7_1),
+  .sram_rdata_a8_1(sram_rdata_a8_1),
 
+  .sram_raddr_a0(sram_raddr_a0),
+  .sram_raddr_a1(sram_raddr_a1),
+  .sram_raddr_a2(sram_raddr_a2),
+  .sram_raddr_a3(sram_raddr_a3),
+  .sram_raddr_a4(sram_raddr_a4),
+  .sram_raddr_a5(sram_raddr_a5),
+  .sram_raddr_a6(sram_raddr_a6),
+  .sram_raddr_a7(sram_raddr_a7),
+  .sram_raddr_a8(sram_raddr_a8),
+/* SRAM B */
+  .sram_rdata_b0(sram_rdata_b0),
+  .sram_rdata_b1(sram_rdata_b1),
+  .sram_rdata_b2(sram_rdata_b2),
+  .sram_rdata_b3(sram_rdata_b3),
+  .sram_rdata_b4(sram_rdata_b4),
+  .sram_rdata_b5(sram_rdata_b5),
+  .sram_rdata_b6(sram_rdata_b6),
+  .sram_rdata_b7(sram_rdata_b7),
+  .sram_rdata_b8(sram_rdata_b8),
+/* SRAM B set 2 */
+  .sram_rdata_b0_1(sram_rdata_b0_1),
+  .sram_rdata_b1_1(sram_rdata_b1_1),
+  .sram_rdata_b2_1(sram_rdata_b2_1),
+  .sram_rdata_b3_1(sram_rdata_b3_1),
+  .sram_rdata_b4_1(sram_rdata_b4_1),
+  .sram_rdata_b5_1(sram_rdata_b5_1),
+  .sram_rdata_b6_1(sram_rdata_b6_1),
+  .sram_rdata_b7_1(sram_rdata_b7_1),
+  .sram_rdata_b8_1(sram_rdata_b8_1),
 
-  lenet_2set U0(.clk(clk), .rst_n(rst_n), .boot_up(boot_up), 
-              .boot_addr(boot_addr), .boot_datai(boot_datai), .boot_web(boot_web), 
-              .peri_web(peri_web), .peri_addr(peri_addr), .peri_datao(peri_datao));
+  .sram_raddr_b0(sram_raddr_b0),
+  .sram_raddr_b1(sram_raddr_b1),
+  .sram_raddr_b2(sram_raddr_b2),
+  .sram_raddr_b3(sram_raddr_b3),
+  .sram_raddr_b4(sram_raddr_b4),
+  .sram_raddr_b5(sram_raddr_b5),
+  .sram_raddr_b6(sram_raddr_b6),
+  .sram_raddr_b7(sram_raddr_b7),
+  .sram_raddr_b8(sram_raddr_b8),
+
+  .sram_write_enable_b0(sram_write_enable_b0),
+  .sram_write_enable_b1(sram_write_enable_b1),
+  .sram_write_enable_b2(sram_write_enable_b2),
+  .sram_write_enable_b3(sram_write_enable_b3),
+  .sram_write_enable_b4(sram_write_enable_b4),
+  .sram_write_enable_b5(sram_write_enable_b5),
+  .sram_write_enable_b6(sram_write_enable_b6),
+  .sram_write_enable_b7(sram_write_enable_b7),
+  .sram_write_enable_b8(sram_write_enable_b8),
+
+  .sram_bytemask_b(sram_bytemask_b),
+  .sram_waddr_b(sram_waddr_b),
+  .sram_wdata_b(sram_wdata_b),
+  //set 2
+  .sram_wdata_b_1(sram_wdata_b_1),
+/* SRAM C */
+  .sram_rdata_c0(sram_rdata_c0),
+  .sram_rdata_c1(sram_rdata_c1),
+  .sram_rdata_c2(sram_rdata_c2),
+  .sram_rdata_c3(sram_rdata_c3),
+  .sram_rdata_c4(sram_rdata_c4),
+/* SRAM C set 2 */
+  .sram_rdata_c0_1(sram_rdata_c0_1),
+  .sram_rdata_c1_1(sram_rdata_c1_1),
+  .sram_rdata_c2_1(sram_rdata_c2_1),
+  .sram_rdata_c3_1(sram_rdata_c3_1),
+  .sram_rdata_c4_1(sram_rdata_c4_1),
+
+  .sram_raddr_c0(sram_raddr_c0),
+  .sram_raddr_c1(sram_raddr_c1),
+  .sram_raddr_c2(sram_raddr_c2),
+  .sram_raddr_c3(sram_raddr_c3),
+  .sram_raddr_c4(sram_raddr_c4),
+
+  .sram_write_enable_c0(sram_write_enable_c0),
+  .sram_write_enable_c1(sram_write_enable_c1),
+  .sram_write_enable_c2(sram_write_enable_c2),
+  .sram_write_enable_c3(sram_write_enable_c3),
+  .sram_write_enable_c4(sram_write_enable_c4),
+
+  .sram_bytemask_c(sram_bytemask_c),
+  .sram_waddr_c(sram_waddr_c),
+  .sram_wdata_c(sram_wdata_c),
+  //set 2
+  .sram_wdata_c_1(sram_wdata_c_1),
+/* SRAM D */
+  .sram_rdata_d0(sram_rdata_d0),
+  .sram_rdata_d1(sram_rdata_d1),
+  .sram_rdata_d2(sram_rdata_d2),
+  .sram_rdata_d3(sram_rdata_d3),
+  .sram_rdata_d4(sram_rdata_d4),
+/* SRAM D set 2 */
+  .sram_rdata_d0_1(sram_rdata_d0_1),
+  .sram_rdata_d1_1(sram_rdata_d1_1),
+  .sram_rdata_d2_1(sram_rdata_d2_1),
+  .sram_rdata_d3_1(sram_rdata_d3_1),
+  .sram_rdata_d4_1(sram_rdata_d4_1),
+
+  .sram_raddr_d0(sram_raddr_d0),
+  .sram_raddr_d1(sram_raddr_d1),
+  .sram_raddr_d2(sram_raddr_d2),
+  .sram_raddr_d3(sram_raddr_d3),
+  .sram_raddr_d4(sram_raddr_d4),
+
+  .sram_write_enable_d0(sram_write_enable_d0),
+  .sram_write_enable_d1(sram_write_enable_d1),
+  .sram_write_enable_d2(sram_write_enable_d2),
+  .sram_write_enable_d3(sram_write_enable_d3),
+  .sram_write_enable_d4(sram_write_enable_d4),
+
+  .sram_bytemask_d(sram_bytemask_d),
+  .sram_waddr_d(sram_waddr_d),
+  .sram_wdata_d(sram_wdata_d),
+  // set 2
+  .sram_wdata_d_1(sram_wdata_d_1),
+/* SRAM E */
+  .sram_rdata_e0(sram_rdata_e0),
+  .sram_rdata_e1(sram_rdata_e1),
+  .sram_rdata_e2(sram_rdata_e2),
+  .sram_rdata_e3(sram_rdata_e3),
+  .sram_rdata_e4(sram_rdata_e4),
+/* SRAM E set 2 */
+  .sram_rdata_e0_1(sram_rdata_e0_1),
+  .sram_rdata_e1_1(sram_rdata_e1_1),
+  .sram_rdata_e2_1(sram_rdata_e2_1),
+  .sram_rdata_e3_1(sram_rdata_e3_1),
+  .sram_rdata_e4_1(sram_rdata_e4_1),
+
+  .sram_raddr_e0(sram_raddr_e0),
+  .sram_raddr_e1(sram_raddr_e1),
+  .sram_raddr_e2(sram_raddr_e2),
+  .sram_raddr_e3(sram_raddr_e3),
+  .sram_raddr_e4(sram_raddr_e4),
+
+  .sram_write_enable_e0(sram_write_enable_e0),
+  .sram_write_enable_e1(sram_write_enable_e1),
+  .sram_write_enable_e2(sram_write_enable_e2),
+  .sram_write_enable_e3(sram_write_enable_e3),
+  .sram_write_enable_e4(sram_write_enable_e4),
+
+  .sram_bytemask_e(sram_bytemask_e),
+  .sram_waddr_e(sram_waddr_e),
+  .sram_wdata_e(sram_wdata_e),
+// set 2
+  .sram_wdata_e_1(sram_wdata_e_1),
+/* SRAM F */
+  .sram_bytemask_f(sram_bytemask_f),
+  .sram_waddr_f(sram_waddr_f),
+  .sram_wdata_f(sram_wdata_f),
+  .sram_wdata_f_1(sram_wdata_f_1),
+  .sram_write_enable_f(sram_write_enable_f),
+/* CONV WEIGHT SRAM */
+  .conv_sram_raddr_weight(conv_sram_raddr_weight),
+  .conv_sram_rdata_weight(conv_sram_rdata_weight),
+/* FC WEIGHT SRAM */
+  .fc_sram_raddr_weight(fc_sram_raddr_weight),
+  .fc_sram_rdata_weight(fc_sram_rdata_weight)
+);
   
 endmodule
