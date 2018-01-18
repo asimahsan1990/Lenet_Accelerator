caca84be9c5982a6a19bceb62b48d74242a3c144
Paul Lai <paul116e@gmail.com>
2018-01-18 20:21:58 +0800

    update G07

---
+++ G07/APR/posim/post_sim.f
@@ -0,0 +1,10 @@
+test_post_sim.v
+../icc/post_layout/lenet_layout.v
+
+-v /usr/cadtool/SAED32_EDK/lib/stdcell_hvt/verilog/saed32nm_hvt.v
+
+sram_model/sram_256x32b.v
+sram_model/sram_20250x80b.v
+sram_model/sram_20000x100b.v
+
++access+r
